8fec9c80412607f25b7a6e81bd90c6f7044dbd2f
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-01-06 22:04:34 +0300

    #

--- Transfer.Web/Startup.cs
+++ Transfer.Web/Startup.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Transfer.Common;
+using Transfer.Dal.Context;
 
 namespace Transfer.Web
 {
@@ -24,13 +23,19 @@ namespace Transfer.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
             services.AddMvc();
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transfer API", Version = "v1" });
             });
+
+            //параметры приложения
+            var settings = Configuration.GetSection("appSettings").Get<TransferSettings>();
+
+
+            //подключение к БД
+            services.AddDbContext<TransferContext>(options =>
+                options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("TransferDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
