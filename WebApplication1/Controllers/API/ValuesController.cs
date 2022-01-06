3e930cf21da5115d6f2ec64054cc645b1fb70d42
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-01-06 19:14:54 +0300

    #

---
+++ WebApplication1/Controllers/API/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetData/{someData}")]
+        public async Task<long> GetSomeData([Required] long someData)
+        {
+            var res = await Task.Run(() => someData * 2);
+            return res;
+        }
+    }
+}
