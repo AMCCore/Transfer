6405b4447b5a989ddd4c2039ab45d710f7232b87
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-01-08 22:59:05 +0300

    #

---
+++ Transfer.Common/Cache/ICacheService.cs
@@ -0,0 +1,18 @@
+﻿namespace Transfer.Common.Cache
+{
+    /// <summary>
+    /// интерфейс хранения в кэше
+    /// </summary>
+    public interface ICacheService
+    {
+        /// <summary>
+        /// достать из кэша
+        /// </summary>
+        T Get<T>(string cacheKey) where T : class;
+
+        /// <summary>
+        /// положить в кэш
+        /// </summary>
+        void Set<T>(string cacheKey, T data) where T : class;
+    }
+}
