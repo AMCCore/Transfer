bf4bf8e34dcc973a4a98937a2cc8cc3bb0dff99d
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-04-11 20:43:33 +0300

    #

--- Transfer.Console/Program.cs
+++ Transfer.Console/Program.cs
@@ -11,7 +11,8 @@ internal class Program
     {
         System.Console.WriteLine("Hello World!");
 
-        using var uc = new UnitOfWork("Data Source=31.31.196.202;Initial Catalog=u1617627_trs;Integrated Security=False;User Id=u1617627_trs;Password=1Ov8b@o9;");
+        using var uc = new UnitOfWork("Data Source=31.31.196.202;Initial Catalog=u0283737_trs;Integrated Security=False;User Id=u0283737_trs;Password=7bcB8$1y;");
+        //using var uc = new UnitOfWork("Data Source=31.31.196.202;Initial Catalog=u1617627_trs;Integrated Security=False;User Id=u1617627_trs;Password=1Ov8b@o9;");
 
         var admin = uc.GetSet<DbAccount>().FirstOrDefault(x => x.Id == Guid.Parse("CC8EFEFA-0D2E-49FF-B982-6E1EDAED2C76"));
         admin.Password = BCrypt.Net.BCrypt.HashString("jopa");
