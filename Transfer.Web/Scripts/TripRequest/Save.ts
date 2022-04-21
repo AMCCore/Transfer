077389f43b2f4018e33971471a5a4be8c2eda747
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-04-21 14:29:22 +0300

    #

---
+++ Transfer.Web/Scripts/TripRequest/Save.ts
@@ -0,0 +1,7 @@
+﻿$(() => {
+    $(".basicAutoComplete").select2({
+        minimumInputLength: 3,
+        placeholder: "Select a state",
+        allowClear: true
+    });
+});
---
+++ Transfer.Web/wwwroot/js/TripRequest/Save.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".basicAutoComplete").select2({
+        minimumInputLength: 3,
+        placeholder: "Select a state",
+        allowClear: true
+    });
+});
+//# sourceMappingURL=Save.js.map
