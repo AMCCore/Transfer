d88cbb4d329febd8f98ba3c280171362b2fcae2a
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-05-04 13:37:52 +0300

    #

---
+++ Transfer.Web/Controllers/API/FIASController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Transfer.Web.Models;
+using Transfer.Web.Models.Fias;
+
+namespace Transfer.Web.Controllers.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FIASController : ControllerBase
+{
+    private static readonly string BasicUrl = "https://papi.mos.ru/api/fias/7.0";
+
+    /// <summary>
+    /// поиск адреса в ФИАС (прокси класс)
+    /// </summary>
+    /// <param name="Query"></param>
+    [HttpGet]
+    public async Task<IActionResult> SearchHome(string Query)
+    {
+        var req = new FIASRequest()
+        {
+            Query = Query,
+            Count = 20,
+            LocationsBoost = new FIASRequestLocation[1] { new FIASRequestLocation { Region = "Москва" } }
+        };
+        using (var httpClient = new HttpClient())
+        {
+            var jsonRequest = JsonConvert.SerializeObject(req);
+            using (var requestMessage = CreateHttpRequestMessage(HttpMethod.Post, new Uri($"{BasicUrl}/searchHome"), jsonRequest))
+            {
+                using (var response = await httpClient.SendAsync(requestMessage))
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var json = JObject.Parse(responseData);
+                    return Ok(json);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Формирование запроса
+    /// </summary>
+    /// <returns></returns>
+    private static HttpRequestMessage CreateHttpRequestMessage(HttpMethod method, Uri url, string requestContent = null)
+    {
+        var requestMessage = new HttpRequestMessage(method, url);
+        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ETPToken.Token);
+        if (!string.IsNullOrWhiteSpace(requestContent))
+        {
+            requestMessage.Content = new StringContent(requestContent, Encoding.UTF8, "application/json");
+        }
+
+        return requestMessage;
+    }
+}
--- Transfer.Web/wwwroot/js/TripRequest/Save.js
+++ Transfer.Web/wwwroot/js/TripRequest/Save.js
@@ -1,9 +1,42 @@
 $(function () {
-    //$(".basicAutoComplete").select2({
-    //    minimumInputLength: 3,
-    //    placeholder: "Select a state",
-    //    allowClear: true,
-    //    theme: 'bootstrap-5'
-    //});
+    $(".input-addr").select2({
+        minimumInputLength: 3,
+        placeholder: "Начните вводить адрес",
+        allowClear: true,
+        theme: 'bootstrap-5',
+        selectOnClose: true,
+        ajax: {
+            url: '/api/FIAS',
+            data: function (params) {
+                var query = {
+                    query: params.term,
+                    //withParent: 1,
+                    //limit: 20,
+                    //contentType: 'street',
+                    //cityId: 7700000000000
+                };
+                return query;
+            },
+            type: 'GET',
+            processResults: function (data) {
+                var result = [];
+                for (var i = 0; i < data.suggestions.length; i++) {
+                    if (data.suggestions[i].data.fias_id) {
+                        result.push({
+                            id: data.suggestions[i].data.fias_id,
+                            text: data.suggestions[i].value,
+                            district: data.suggestions[i].data.district,
+                            region: data.suggestions[i].data.adm_area
+                        });
+                    }
+                }
+                return {
+                    results: result
+                };
+            },
+            cache: true,
+        }
+    }).on('change', function (e) {
+    });
 });
 //# sourceMappingURL=Save.js.map
