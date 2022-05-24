4d9bf8b47aef99456c729e06a647fc594036983a
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-05-24 19:12:28 +0300

    #

--- Transfer.Bot/Actions/SendMsgToUser.cs
+++ Transfer.Bot/Actions/SendMsgToUser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 using Transfer.Bot.Dtos;
 
 namespace Transfer.Bot.Actions;
@@ -9,9 +10,19 @@ public static class SendMsgToUser
 {
     public async static Task<Message> SendMessageToUser(this ITelegramBotClient bot, SendMsgToUserDto message)
     {
+        if (string.IsNullOrWhiteSpace(message.Link))
+        {
+            return await bot.SendTextMessageAsync(
+                chatId: message.ChatId,
+                text: message.Message,
+                replyMarkup: Menu.BaseMenu.backtomenu);
+        }
+
+        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl(message.LinkName ?? message.Link, message.Link));
+
         return await bot.SendTextMessageAsync(
             chatId: message.ChatId,
             text: message.Message,
-            replyMarkup: Menu.BaseMenu.backtomenu);
+            replyMarkup: keyboard);
     }
 }
