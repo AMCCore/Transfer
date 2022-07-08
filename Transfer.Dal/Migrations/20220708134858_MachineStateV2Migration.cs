3a8f3be77b1ce20836beb8906186f5c7add4afc0
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-07-08 16:51:02 +0300

    #

---
+++ Transfer.Dal/Migrations/20220708134858_MachineStateV2Migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transfer.Dal.Migrations
+{
+    public partial class MachineStateV2Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateValid",
+                table: "TripRequestReplays");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseSystem",
+                table: "StateMachineStates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseSystem",
+                table: "StateMachineStates");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateValid",
+                table: "TripRequestReplays",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
