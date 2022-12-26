6a41d9f1af6aa768e94429cc06a09911bcf0d85e
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2022-12-26 23:06:33 +0300

    #

---
+++ Transfer.Bl/Dto/AccountWithPassDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfer.Bl.Dto;
+
+public class AccountWithPassDto : AccountDto
+{
+    public string Password { get; set; }
+}
---
+++ Transfer.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Transfer.Common.Settings;
+using Transfer.Common;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Transfer.Bl.Dto;
+using Transfer.Common.Enums.AccessRights;
+using System;
+using Transfer.Dal.Entities;
+using Transfer.Dal.Migrations;
+using Transfer.Common.Extensions;
+
+namespace Transfer.Web.Controllers.API;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class AdminController : ControllerBase
+{
+    private readonly IWebHostEnvironment _appEnvironment;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public AdminController(IWebHostEnvironment appEnvironment, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _appEnvironment = appEnvironment;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    [Route(nameof(CheckHealth))]
+    public IActionResult CheckHealth()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route(nameof(AddAdminUser))]
+    public async Task<IActionResult> AddAdminUser([FromBody] AccountWithPassDto account)
+    {
+        if (!Moduls.Security.Current.HasRightForSomeOrganisation(AdminAccessRights.IsAdmin))
+        {
+            return Unauthorized();
+        }
+
+        await _unitOfWork.BeginTransactionAsync();
+
+        var pd = new DbPersonData
+        {
+            Id = Guid.NewGuid(),
+            FirstName = account.FirstName,
+            LastName = account.LastName,
+            MiddleName = account.MiddleName ?? string.Empty,
+            BirthDate = account.BirthDate ?? new DateTime(1900, 3, 6),
+            IsMale = true,
+            DocumentSeries = string.Empty,
+            DocumentNumber = string.Empty,
+            DocumentSubDivisionCode = string.Empty,
+            DocumentIssurer = string.Empty,
+            DocumentDateOfIssue = DateTime.MinValue,
+            RegistrationAddress = string.Empty
+        };
+
+        await _unitOfWork.AddEntityAsync(pd);
+
+        var acc = new DbAccount
+        {
+            Id = Guid.NewGuid(),
+            PersonDataId = pd.Id,
+            LastUpdateTick = DateTime.Now.Ticks,
+            DateCreated = DateTime.Now,
+            Password = BCrypt.Net.BCrypt.HashString(account.Password),
+            Email = account.Email,
+        };
+
+        await _unitOfWork.AddEntityAsync(acc);
+
+        await _unitOfWork.AddEntityAsync(new DbAccountRight
+        {
+            Id = Guid.NewGuid(),
+            AccountId = acc.Id,
+            RightId = AdminAccessRights.IsAdmin.GetEnumGuid(),
+            OrganisationId = null
+        });
+
+        await _unitOfWork.CommitAsync();
+
+        return Ok(acc.Id);
+    }
+}
