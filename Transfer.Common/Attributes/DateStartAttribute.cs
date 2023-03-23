54a396433757a8a9d12afb08dacee502ba1a2b9a
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2023-03-24 00:16:19 +0300

    #

---
+++ Transfer.Common/Attributes/DateStartAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfer.Common.Attributes;
+
+public sealed class DateStartAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        DateTime dateStart = (DateTime)value;
+        return (dateStart > DateTime.Now);
+    }
+}
