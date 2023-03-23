98c378d1697e1efdd4911d596401fd70994de852
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2023-03-23 12:01:22 +0300

    #

---
+++ Transfer.Common/Attributes/DateEndAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfer.Common.Attributes;
+
+public sealed class DateEndAttribute : ValidationAttribute
+{
+    public string DateStartProperty { get; set; }
+
+    //в разработке
+    public override bool IsValid(object value)
+    {
+        return false;
+
+        // Get Value of the DateStart property
+        //string dateStartString = HttpContext.Current.Request[DateStartProperty];
+        //DateTime dateEnd = (DateTime)value;
+        //DateTime dateStart = DateTime.Parse(dateStartString);
+
+        //// Meeting start time must be before the end time
+        //return dateStart < dateEnd;
+    }
+}
